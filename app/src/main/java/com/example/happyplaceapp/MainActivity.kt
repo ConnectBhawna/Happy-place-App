2832724027b75c2b4dd026da46c0378e77f109e6
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-04-26 19:34:28 +0530

    Error and all

--- app/src/main/java/com/example/happyplaceapp/MainActivity.kt
+++ app/src/main/java/com/example/happyplaceapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.happyplaceapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val fabAddHappyPlace = findViewById<FloatingActionButton>(R.id.fabAddHappyPlace)
+        fabAddHappyPlace.setOnClickListener{
+            val intent = Intent(this,AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
