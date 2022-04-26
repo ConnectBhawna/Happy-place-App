d04799bcb65fbfb99c1582960088c4bc3e7c8952
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-04-26 13:23:51 +0530

    Done with Splash Screen part

---
+++ app/src/main/java/com/example/happyplaceapp/SplashScreen.kt
@@ -0,0 +1,32 @@
+package com.example.happyplaceapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        //this one is for hiding taskBar
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+
+        //variables for animation
+        // Animation topAnim,bottomAnim
+        val backgroundImg : ImageView = findViewById(R.id.iv_logo)
+        val topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation)
+        backgroundImg.startAnimation(topAnim)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },3000)
+    }
+}
