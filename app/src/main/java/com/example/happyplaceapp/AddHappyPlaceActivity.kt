b5260420c321b2fa31734ce51bdbf946a95bdcb4
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-04-30 10:28:03 +0530

    Done with style feature

--- app/src/main/java/com/example/happyplaceapp/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplaceapp/AddHappyPlaceActivity.kt
@@ -8,6 +8,7 @@ class AddHappyPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+
         setSupportActionBar(toolbar_add_place)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toolbar_add_place.setNavigationOnClickListener {
