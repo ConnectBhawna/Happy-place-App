2f80ca9f787d994ae3d555a128b20032b0b159d1
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-05-07 12:43:45 +0530

    Storing data in recycler View

--- app/src/main/java/com/example/happyplaceapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaceapp/activities/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.happyplaceapp.R
+import com.example.happyplaceapp.adapters.HappyPlacesAdapter
 import com.example.happyplaceapp.database.DatabaseHandler
 import com.example.happyplaceapp.models.HappyPlaceModel
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,23 +17,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       // val fabAddHappyPlace = findViewById<FloatingActionButton>(R.id.fabAddHappyPlace)
-        fabAddHappyPlace.setOnClickListener{
+        // val fabAddHappyPlace = findViewById<FloatingActionButton>(R.id.fabAddHappyPlace)
+        fabAddHappyPlace.setOnClickListener {
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
             startActivity(intent)
         }
         getHappyPlacesListFromLocalDB()
     }
+    // TODO ( Creating a function for setting up the recyclerview to UI.)
+
+    /**
+     * A function to populate the recyclerview to the UI.
+     */
+    private fun setupHappyPlacesRecyclerView(happyPlacesList: ArrayList<HappyPlaceModel>) {
+
+        rv_happy_places_list.layoutManager = LinearLayoutManager(this)
+        rv_happy_places_list.setHasFixedSize(true)
+
+        val placesAdapter = HappyPlacesAdapter(this, happyPlacesList)
+        rv_happy_places_list.adapter = placesAdapter
+    }
 
     private fun getHappyPlacesListFromLocalDB(){
         val dbHandler = DatabaseHandler(this)
-        val getHappyPlaceList : ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
+        val getHappyPlacesList : ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
 
-        if(getHappyPlaceList.size > 0){
-            for(i in getHappyPlaceList){
-                Log.e("Title",i.title)
-                Log.e("Description",i.description)
-            }
+        if(getHappyPlacesList.size > 0){
+            rv_happy_places_list.visibility = View.VISIBLE
+            tv_no_records_available.visibility = View.GONE
+            setupHappyPlacesRecyclerView(getHappyPlacesList)
+        }
+        else {
+            rv_happy_places_list.visibility = View.GONE
+            tv_no_records_available.visibility = View.VISIBLE
         }
     }
 }
---
+++ app/src/main/java/com/example/happyplaceapp/adapters/HappyPlacesAdapter.kt
@@ -0,0 +1,68 @@
+package com.example.happyplaceapp.adapters
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.happyplaceapp.R
+import com.example.happyplaceapp.models.HappyPlaceModel
+import kotlinx.android.synthetic.main.item_happy_place.view.*
+
+// TODO  ( Creating an adapter class for binding it to the recyclerview in the new package which is adapters.)
+
+open class HappyPlacesAdapter(
+    private val context: Context,
+    private var list: ArrayList<HappyPlaceModel>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    /**
+     * Inflates the item views which is designed in xml layout file
+     *
+     * create a new
+     * {@link ViewHolder} and initializes some private fields to be used by RecyclerView.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        return MyViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.item_happy_place,
+                parent,
+                false
+            )
+        )
+    }
+
+    /**
+     * Binds each item in the ArrayList to a view
+     *
+     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+     * an item.
+     *
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     */
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+
+        if (holder is MyViewHolder) {
+            holder.itemView.iv_place_image.setImageURI(Uri.parse(model.image))
+            holder.itemView.tvTitle.text = model.title
+            holder.itemView.tvDescription.text = model.description
+        }
+    }
+
+    /**
+     * Gets the number of items in the list
+     */
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    /**
+     * A ViewHolder describes an item view and metadata about its place within the RecyclerView.
+     */
+    private class MyViewHolder(view: View) : RecyclerView.ViewHolder(view)
+}
