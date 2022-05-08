c7370f6e2bcdb468b59ce53a534ef0adc0250296
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-05-08 21:16:02 +0530

    Showing image in new activity

--- app/src/main/java/com/example/happyplaceapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaceapp/activities/MainActivity.kt
@@ -50,11 +50,13 @@ class MainActivity : AppCompatActivity() {
         placesAdapter.setOnClickListener(object :
             HappyPlacesAdapter.OnClickListener {
             override fun onClick(position: Int, model: HappyPlaceModel) {
-                val intent = Intent(this@MainActivity, HappyPlaceDetailActivity::class.java)
+                val intent = Intent(this@MainActivity,
+                    HappyPlaceDetailActivity::class.java)
+                intent.putExtra(EXTRA_PLACE_DETAILS,model)
                 startActivity(intent)
             }
         })
-        // END
+
     }
 
     private fun getHappyPlacesListFromLocalDB(){
@@ -87,6 +89,7 @@ class MainActivity : AppCompatActivity() {
 
     companion object{
         var ADD_PLACE_ACTIVITY_REQUEST_CODE = 1
+        var EXTRA_PLACE_DETAILS = "extra_place_details"
     }
 }
 
